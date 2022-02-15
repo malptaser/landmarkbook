dd0bb1fc091641c3c3a8488140ab33ee7ccd2bda
M.Alp <mtaser@burgan.com.tr>
2022-02-15 19:10:47 +0300

    ViewController içinde arrayler, indexpathrow, rowsinsection pratikleri yapıldı.

--- Landmark Book/ViewController.swift
+++ Landmark Book/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    // landmarkNames adında string arrayı oluşturduk
+    var landmarkNames = [String]()
+    // landmarkImages adında UIImage arrayı oluşturduk
+    var landmarkImages = [UIImage]()
+    
+    
     @IBOutlet weak var tableView: UITableView!
     
     
@@ -18,20 +24,46 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         // Her tableView oluşturulduğunda delegate ve datasource viewControllere eşitlenir.
         tableView.delegate = self
         tableView.dataSource = self
+     
+        
+        // Landmark Book Data
+        
+        // arrayimizi isimlerle doldurduk.
+        landmarkNames.append("roma")
+        landmarkNames.append("wall")
+        landmarkNames.append("stone")
+        landmarkNames.append("tac")
+        landmarkNames.append("kremlin")
+        
+        
+        // arrayimizi fotoğraflar ile doldurduk.
+        landmarkImages.append(UIImage(named: "roma.jpg")!)
+        landmarkImages.append(UIImage(named: "wall.jpg")!)
+        landmarkImages.append(UIImage(named: "stone.jpg")!)
+        landmarkImages.append(UIImage(named: "tac.jpg")!)
+        landmarkImages.append(UIImage(named: "kremlin.jpg")!)
+   
+  
+        
+        
+        
     }
+    
     
     // cellForRowAt ( UITableViewDelegate ve UITableViewDataSource kullandığımız için cellForRowAt + numberOfRowsInSection eklenmeli )
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
      let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
+        // landmarknames[indexPath.row] ile aynı dizeleri eşitleyecek
+        
+        cell.textLabel?.text = landmarkNames[indexPath.row]
         return cell
     }
     
-    // numberOfRowsInSection
+    // numberOfRowsInSection kaç satırdan oluşturulacağını belirtir.
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return landmarkNames.count
     }
 }
 
