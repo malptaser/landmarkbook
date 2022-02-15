12bc36055a42a0944486fd0a8d0b09852bf8dc76
M.Alp <mtaser@burgan.com.tr>
2022-02-15 18:53:16 +0300

    TableView oluşturuldu..

--- Landmark Book/ViewController.swift
+++ Landmark Book/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // App baştan başladığında çalışan olaylar viewDidLoad'da uygulanır..
+        // Her tableView oluşturulduğunda delegate ve datasource viewControllere eşitlenir.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    // cellForRowAt ( UITableViewDelegate ve UITableViewDataSource kullandığımız için cellForRowAt + numberOfRowsInSection eklenmeli )
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+    // numberOfRowsInSection
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 }
 
